0bcd06d65e2a7817218fc5d92d117e123c7c33fd
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-02-02 21:49:52 -0600

    route and data

--- pages/todo/[todoid].jsx
+++ pages/todo/[todoid].jsx
@@ -2,15 +2,39 @@ import Link from 'next/link'
 
 import { useRouter } from 'next/router'
 
-export default function Todo(params) {
+export async function getStaticProps(context) { //get only one
+    const {params} = context
+    const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${params.todoid}`)
+    const todo = await data.json()
+    return{
+        props: {todo},
+    } 
+    
+}
 
-    const router = useRouter()
-    const showTodoId = router.query.todoid //todoid is the name of my jsx archive
+export async function getStaticPaths() { //get all data from api
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/')
+    const data = await res.json()
+    const paths = data.map((todo)=>{
+        return{
+            params: {
+                todoid: `${todo.id}`,
+            }
+        }
+    })
+
+    return{paths, fallback: false}
+}
+
+export default function Todo({todo}) {
+
+    /*const router = useRouter()
+    const showTodoId = router.query.todoid //todoid is the name of my jsx archive */
     return (
         <>
-            <Link href="/">Home</Link>
-            <h1>Show todo : {showTodoId}</h1>
-            <p>Coments <Link href={`${showTodoId}/comments/1`}>here</Link></p>
+            <Link href="/todo">Home</Link>
+            <h1>Show todo : {todo.id}</h1>
+            <p>Coments <Link href={`${todo.id}/comments/1`}>here</Link></p>
         </>
     )
 }
--- pages/todo/index.jsx
+++ pages/todo/index.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 
 export async function getStaticProps(params) {
     const data = await fetch('https://jsonplaceholder.typicode.com/todos')
@@ -16,7 +18,7 @@ export default function Todos({todos}) {
             <h1>My Task to do:</h1>
             <ul>
                 {todos.map((todo)=>(
-                    <li key={todo.id}>{todo.title} -- {todo.id}</li>
+                    <li key={todo.id}>{todo.title} -- {todo.id}<Link href={`todo/${todo.id}`}> Go more</Link> </li>
                 ))}
             </ul>
         </>
